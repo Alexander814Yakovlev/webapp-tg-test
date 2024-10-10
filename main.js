144933a64e586e1ecb895cbbb621c7684c8bad41
Alex <cahek814@gmail.com>
2024-10-10 16:15:36 +0200

    fixed sendData type

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 let tg = window.Telegram.WebApp;
+tg.ready()
 let console = document.querySelector(".console");
 let sendButton = document.querySelector(".button");
 
 console.value = 'Wake up! Neo'
-sendButton.onclick = () => {
+sendButton.addEventListener('click', () => {
+    alert("message has been sent")
     tg.sendData("OK")
     tg.sendData(JSON.stringify({ result: 'ok', data: console.textContent }))
-    alert("message has been sent")
-}
+
+})
