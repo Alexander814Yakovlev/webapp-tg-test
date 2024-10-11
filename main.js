660bbc3abd092cd4245d2d86715dcbdaa0b439ef
Alex <cahek814@gmail.com>
2024-10-11 12:18:42 +0200

    fixed sendData, changed user permissions for webapp v 0.12

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const v = 0.11
+const v = 0.12
 
 let version = document.querySelector(".version")
 version.textContent = `VER: ${v}`
@@ -9,8 +9,14 @@ tg.ready()
 let myConsole = document.querySelector(".console");
 let sendButton = document.querySelector(".button");
 
-myConsole.value = 'Wake up! Neo'
-sendButton.addEventListener('click', () => {
-    tg.showPopUp("message has been sent")
-    tg.sendData(JSON.stringify({ result: 'ok', data: myConsole.value }))
-})
+if (tg.initDataUnsafe.user.id === 307971589) {
+    myConsole.value = 'Wake up! Neo'
+    sendButton.addEventListener('click', () => {
+        tg.showPopUp("message has been sent")
+        tg.sendData(JSON.stringify({ result: 'ok', data: myConsole.value }))
+    })
+} else {
+    myConsole.style.backgroundColor = '#ff0000'
+    myConsole.value = `${tg.initDataUnsafe.user.first_name}! У Вас нет доступа к этому приложению!`
+    sendButton.hidden = true
+}
